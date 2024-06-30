d4d96609637884e7c408112bd78f87414c127132
bbetul09 <125596358+bbetul09@users.noreply.github.com>
2024-06-30 16:15:10 +0300

    D

---
+++ yeni-portfolio/react-portfolio/src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Layout from './components/Layout'
+import About from './components/About'
+import Contact from './components/Contact'
+
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+         
+        
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
